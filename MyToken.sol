d9d48468c155a1d019fc5752d8b70477ffb28a9a
Ghostboy007 <96197284+Ghostboy007@users.noreply.github.com>
2024-06-26 09:41:43 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,34 +1,43 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.18;
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
 
 contract MyToken {
 
-    string public name;
-    string public symbol;
-    uint256 public totalSupply;
-    mapping(address => uint256) public balances;
+  
+    // public variables here
+string public tokenName = "META";
+string public tokenAbbrv = "MTA";
+uint public totalSupply = 0;
 
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _initialSupply;
-        balances[msg.sender] = _initialSupply;
+mapping(address => uint ) public balances;
+
+function mint (address  _address , uint _value ) public {
+
+    totalSupply += _value ;
+    balances[_address] += _value;
+}
+
+function burn (address _address , uint _value) public {
+    if (balances[_address] >= _value) {
+        totalSupply -= _value ;
+        balances[_address] -= _value;
     }
+    
+}
 
-    function mint(address _to, uint256 _amount) public {
-        require(_to != address(0), "Mint to the zero address");
-        totalSupply += _amount;
-        balances[_to] += _amount;
-        emit Transfer(address(0), _to, _amount);
-    }
 
-    function burn(address _from, uint256 _amount) public {
-        require(_from != address(0), "Burn from the zero address");
-        require(balances[_from] >= _amount, "Insufficient balance to burn");
-        totalSupply -= _amount;
-        balances[_from] -= _amount;
-        emit Transfer(_from, address(0), _amount);
-    }
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
 }
